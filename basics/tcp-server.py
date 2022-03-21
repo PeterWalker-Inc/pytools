e7af9f795b387fa13a2fd84cf269623898d63748
Peter Walker <peterwalker0695@gmail.com>
2022-03-21 18:10:32 +0530

    minor changes

--- basics/tcp-server.py
+++ basics/tcp-server.py
@@ -8,21 +8,21 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
     server.listen(5)
-    print(f'[*] Listening on {host}:{port}')
-
+    print(f"[*] Listening on {host}:{port}")
+    
     while True:
         client, address = server.accept()
-        print(f"Client: {client}")
-        print(f"Address: {address}")
         print(f"[*] Accepted connections from {address[0]}:{address[1]}")
         client_handler = threading.Thread(target=handle_client, args=(client,))
         client_handler.start()
+        print("Thread created")
 
 def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(4096)
-        print(f"[*] Recieved {request.decode()}")
+        print(f"[*] Recieved: {request.decode()}")
         sock.send(b'ACK')
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
